e6445828cc59065cc3c180bb26fb3fb55464046e
Haitao Chen <haitch@microsoft.com>
2020-03-01 16:53:12 -0800

    more thought

--- task_store.go
+++ task_store.go
@@ -19,26 +19,49 @@ const StateCompleted State = "Completed"
 // StateFailed indicate task failed.
 const StateFailed State = "Failed"
 
-// TaskStatus is a handle to the
+// StateCanceled indicate task got canceled.
+const StateCanceled State = "Canceled"
+
+// TaskStatus is a handle to the running function.
+// which you can use to wait, cancel, get the result.
 type TaskStatus struct {
 	context.Context
-	State      State
-	Result     interface{}
-	Error      error
+	state      State
+	result     interface{}
+	err        error
 	cancelFunc context.CancelFunc
 	waitGroup  *sync.WaitGroup
+}
+
+// State return state of the task.
+func (t *TaskStatus) State() State {
+	return t.state
 }
 
 // Cancel abort the task execution
 // !! only if the function provided handles context cancel.
 func (t *TaskStatus) Cancel() {
 	t.cancelFunc()
+
+	// could have a debate wait or not
+	// my point is wait drives this TaskStatus to a terminal(consistence) state.
+	//	 you can keep Cancel() or Wait() or State() on this handle, and nothing changes.
+	// without this wait
+	//   1. nothing blocks invoking Wait() on this handle after Cancel()
+	//   2. the state, result, error, can be non-deterministic.
 	t.waitGroup.Wait()
+
+	t.state = StateCanceled
 }
 
 // Wait block current thread/routine until task finished or failed.
-func (t *TaskStatus) Wait() {
+func (t *TaskStatus) Wait() (interface{}, error) {
 	t.waitGroup.Wait()
+
+	// we create new context when starting task, now release it.
+	t.cancelFunc()
+
+	return t.result, t.err
 }
 
 // StartTask returns you a handle which you can Wait or Cancel.
@@ -48,8 +71,8 @@ func StartTask(ctx context.Context, task func(ctx context.Context) (interface{},
 	wg.Add(1)
 	record := &TaskStatus{
 		Context:    ctx,
-		State:      StateRunning,
-		Result:     nil,
+		state:      StateRunning,
+		result:     nil,
 		cancelFunc: cancel,
 		waitGroup:  wg,
 	}
@@ -63,20 +86,20 @@ func runAndTrackTask(record *TaskStatus, task func(ctx context.Context) (interfa
 	defer func() {
 		if r := recover(); r != nil {
 			err := fmt.Errorf("Panic cought: %v, StackTrace: %s", r, debug.Stack())
-			record.State = StateFailed
-			record.Error = err
+			record.state = StateFailed
+			record.err = err
 			record.waitGroup.Done()
 		}
 	}()
 
 	result, err := task(record)
 	if err != nil {
-		record.State = StateFailed
-		record.Error = err
+		record.state = StateFailed
+		record.err = err
 		record.waitGroup.Done()
 	} else {
-		record.State = StateCompleted
-		record.Result = result
+		record.state = StateCompleted
+		record.result = result
 		record.waitGroup.Done()
 	}
 }
--- task_store_test.go
+++ task_store_test.go
@@ -30,13 +30,14 @@ func TestEasyCase(t *testing.T) {
 	ctx := context.TODO()
 	t1 := taskstore.StartTask(ctx, countingTask)
 
-	assert.Equal(t, taskstore.StateRunning, t1.State, "Task should queued to Running")
+	assert.Equal(t, taskstore.StateRunning, t1.State(), "Task should queued to Running")
 
-	time.Sleep(time.Second * 3)
+	rawResult, err := t1.Wait()
+	assert.NoError(t, err)
 
-	assert.Equal(t, taskstore.StateCompleted, t1.State, "Task should complete by now")
-	assert.NotNil(t, t1.Result)
-	result := t1.Result.(int)
+	assert.Equal(t, taskstore.StateCompleted, t1.State(), "Task should complete by now")
+	assert.NotNil(t, rawResult)
+	result := rawResult.(int)
 	assert.Equal(t, result, 9)
 }
 
@@ -45,14 +46,17 @@ func TestCancelFunc(t *testing.T) {
 	ctx := context.TODO()
 	t1 := taskstore.StartTask(ctx, countingTask)
 
-	assert.Equal(t, taskstore.StateRunning, t1.State, "Task should queued to Running")
+	assert.Equal(t, taskstore.StateRunning, t1.State(), "Task should queued to Running")
 
 	time.Sleep(time.Second * 1)
 	t1.Cancel()
 
-	assert.Equal(t, taskstore.StateCompleted, t1.State, "Task should complete by now")
-	assert.NotNil(t, t1.Result)
-	result := t1.Result.(int)
+	rawResult, err := t1.Wait()
+	assert.NoError(t, err)
+
+	assert.Equal(t, taskstore.StateCanceled, t1.State(), "Task should complete by now")
+	assert.NotNil(t, rawResult)
+	result := rawResult.(int)
 	assert.Less(t, result, 9)
 
 	// cancel a task shouldn't cancel it's parent context.
@@ -62,21 +66,6 @@ func TestCancelFunc(t *testing.T) {
 	case <-ctx.Done():
 		fmt.Println("parent ctx got canceled")
 	}
-}
-
-func TestWaitTask(t *testing.T) {
-	t.Parallel()
-	ctx := context.TODO()
-	t1 := taskstore.StartTask(ctx, countingTask)
-
-	assert.Equal(t, taskstore.StateRunning, t1.State, "Task should queued to Running")
-
-	t1.Wait()
-
-	assert.Equal(t, taskstore.StateCompleted, t1.State, "Task should complete by now")
-	assert.NotNil(t, t1.Result)
-	result := t1.Result.(int)
-	assert.Equal(t, result, 9)
 }
 
 func TestDummy(t *testing.T) {
