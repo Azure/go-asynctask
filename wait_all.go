5d00236b17a6bb475839b0eba267e26682a19bd1
Haitao Chen <haidaocht@gmail.com>
2020-04-24 16:36:48 -0700

    WaitAll with options

--- wait_all.go
+++ wait_all.go
@@ -1,8 +1,19 @@
 package asynctask
 
+import (
+	"context"
+	"fmt"
+)
+
+// WaitAllOptions defines options for WaitAll function
+type WaitAllOptions struct {
+	// FailFast set to true will indicate WaitAll to return on first error it sees.
+	FailFast bool
+}
+
 // WaitAll block current thread til all task finished.
 // it return immediately after receive first error.
-func WaitAll(tasks ...*TaskStatus) error {
+func WaitAll(ctx context.Context, options *WaitAllOptions, tasks ...*TaskStatus) error {
 	errorCh := make(chan error)
 	errorChClosed := false
 	defer func() {
@@ -22,16 +33,36 @@ func WaitAll(tasks ...*TaskStatus) error {
 		runningTasks++
 	}
 
+	var errList []error
 	for {
 		select {
 		case err := <-errorCh:
 			runningTasks--
 			if err != nil && isErrorReallyError(err) {
-				return err
+				// return immediately after receive first error.
+				if options.FailFast {
+					return err
+				}
+
+				errList = append(errList, err)
 			}
-			if runningTasks == 0 {
-				return nil
-			}
+		case <-ctx.Done():
+			return fmt.Errorf("WaitAll context canceled. %w", context.Canceled)
+		}
+
+		// are we finished yet?
+		if runningTasks == 0 {
+			break
 		}
 	}
+
+	// we have at least 1 error, return first one.
+	// caller can get error for individual task by using Wait(),
+	// it would return immediately after this WaitAll()
+	if len(errList) > 0 {
+		return errList[0]
+	}
+
+	// no error at all.
+	return nil
 }
--- wait_all_test.go
+++ wait_all_test.go
@@ -17,10 +17,34 @@ func TestWaitAll(t *testing.T) {
 	completedTsk := asynctask.NewCompletedTask()
 
 	start := time.Now()
-	err := asynctask.WaitAll(countingTsk1, countingTsk2, countingTsk3, completedTsk)
+	err := asynctask.WaitAll(ctx, &asynctask.WaitAllOptions{FailFast: true}, countingTsk1, countingTsk2, countingTsk3, completedTsk)
 	elapsed := time.Since(start)
 	assert.NoError(t, err)
+	// should only finish after longest task.
 	assert.True(t, elapsed > 10*200*time.Millisecond)
+}
+
+func TestWaitAllFailFastCase(t *testing.T) {
+	t.Parallel()
+	ctx := newTestContext(t)
+	countingTsk := asynctask.Start(ctx, getCountingTask(10, 200*time.Millisecond))
+	errorTsk := asynctask.Start(ctx, getErrorTask("expected error", 10*time.Millisecond))
+	panicTsk := asynctask.Start(ctx, getPanicTask(20*time.Millisecond))
+	completedTsk := asynctask.NewCompletedTask()
+
+	start := time.Now()
+	err := asynctask.WaitAll(ctx, &asynctask.WaitAllOptions{FailFast: true}, countingTsk, errorTsk, panicTsk, completedTsk)
+	countingTskState := countingTsk.State()
+	panicTskState := countingTsk.State()
+	elapsed := time.Since(start)
+	assert.Error(t, err)
+	assert.Equal(t, "expected error", err.Error())
+	// should fail before we finish panic task
+	assert.True(t, elapsed.Milliseconds() < 15)
+
+	// since we pass FailFast, countingTsk and panicTsk should be still running
+	assert.Equal(t, asynctask.StateRunning, countingTskState)
+	assert.Equal(t, asynctask.StateRunning, panicTskState)
 }
 
 func TestWaitAllErrorCase(t *testing.T) {
@@ -32,9 +56,16 @@ func TestWaitAllErrorCase(t *testing.T) {
 	completedTsk := asynctask.NewCompletedTask()
 
 	start := time.Now()
-	err := asynctask.WaitAll(countingTsk, errorTsk, panicTsk, completedTsk)
+	err := asynctask.WaitAll(ctx, &asynctask.WaitAllOptions{FailFast: false}, countingTsk, errorTsk, panicTsk, completedTsk)
+	countingTskState := countingTsk.State()
+	panicTskState := panicTsk.State()
 	elapsed := time.Since(start)
 	assert.Error(t, err)
 	assert.Equal(t, "expected error", err.Error())
-	assert.True(t, elapsed.Milliseconds() < 15)
+	// should only finish after longest task.
+	assert.True(t, elapsed > 10*200*time.Millisecond)
+
+	// since we pass FailFast, countingTsk and panicTsk should be still running
+	assert.Equal(t, asynctask.StateCompleted, countingTskState, "countingTask should finished")
+	assert.Equal(t, asynctask.StateFailed, panicTskState, "panic task should failed")
 }
