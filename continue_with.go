e0e2282eab0a18321270d440e052576c7a14a9cc
Haitao Chen <haidaocht@gmail.com>
2020-05-06 02:13:18 -0700

    continueWith

---
+++ continue_with.go
@@ -0,0 +1,18 @@
+package asynctask
+
+import "context"
+
+// ContinueFunc is a function that can be connected to previous task with ContinueWith
+type ContinueFunc func(context.Context, interface{}) (interface{}, error)
+
+// ContinueWith start the function when current task is done.
+// result from previous task will be passed in.
+func (tsk *TaskStatus) ContinueWith(ctx context.Context, next ContinueFunc) *TaskStatus {
+	return Start(ctx, func(fCtx context.Context) (interface{}, error) {
+		result, err := tsk.Wait(fCtx)
+		if err != nil {
+			return nil, err
+		}
+		return next(fCtx, result)
+	})
+}
