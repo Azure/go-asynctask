6388b309010bacb930d5283d7f5afc1cd37d95df
Haitao Chen <haitch@microsoft.com>
2022-06-26 02:55:16 -0700

    try afterBoth

---
+++ after_both.go
@@ -0,0 +1,23 @@
+package asynctask
+
+import "context"
+
+// AfterBoth is a function that has 2 input.
+type AfterBothFunc[T, S, R any] func(context.Context, *T, *S) (*R, error)
+
+// AfterBoth runs the function after both 2 input task finished, and will be feed with result from 2 input task.
+func AfterBoth[T, S, R any](ctx context.Context, tskT *Task[T], tskS *Task[S], next AfterBothFunc[T, S, R]) *Task[R] {
+	return Start(ctx, func(fCtx context.Context) (*R, error) {
+		t, err := tskT.Result(fCtx)
+		if err != nil {
+			return nil, err
+		}
+
+		s, err := tskS.Result(fCtx)
+		if err != nil {
+			return nil, err
+		}
+
+		return next(fCtx, t, s)
+	})
+}
