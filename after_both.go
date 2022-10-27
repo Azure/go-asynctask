7df9c4f1552e9b86d8cc0f00c7298f51e16725bd
Haitao Chen <haitch@microsoft.com>
2022-10-27 14:48:30 -0700

    update comments

--- after_both.go
+++ after_both.go
@@ -5,7 +5,8 @@ import "context"
 // AfterBothFunc is a function that has 2 input.
 type AfterBothFunc[T, S, R any] func(context.Context, *T, *S) (*R, error)
 
-// AfterBoth runs the function after both 2 input task finished, and will be feed with result from 2 input task.
+// AfterBoth runs the function after both 2 input task finished, and will be fed with result from 2 input task.
+//   if one of the input task failed, the AfterBoth task will be failed and returned, even other one are still running.
 func AfterBoth[T, S, R any](ctx context.Context, tskT *Task[T], tskS *Task[S], next AfterBothFunc[T, S, R]) *Task[R] {
 	return Start(ctx, func(fCtx context.Context) (*R, error) {
 		t, err := tskT.Result(fCtx)
