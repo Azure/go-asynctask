ddf77a56d49f3f96463ffabe0675b1c286396eff
Xinyue.Wang <Xinyue.Wang@microsoft.com>
2023-06-27 15:50:05 -0700

    add FailFast

--- wait_any.go
+++ wait_any.go
@@ -5,13 +5,23 @@ import (
 	"fmt"
 )
 
+// WaitAnyOptions defines options for WaitAny function
+type WaitAnyOptions struct {
+	// FailFast set to true will indicate WaitAny to return on first error it sees.
+	FailFast bool
+}
+
 // WaitAny block current thread til any of task finished.
-// first error from any tasks passed in will be returned
+// first error from any tasks passed in will be returned if FailFast is set.
 // first task end without error will end wait and return nil
-func WaitAny(ctx context.Context, tasks ...Waitable) error {
+func WaitAny(ctx context.Context, options *WaitAnyOptions, tasks ...Waitable) error {
 	tasksCount := len(tasks)
 	if tasksCount == 0 {
 		return nil
+	}
+
+	if options == nil {
+		options = &WaitAnyOptions{}
 	}
 
 	// tried to close channel before exit this func,
@@ -24,12 +34,38 @@ func WaitAny(ctx context.Context, tasks ...Waitable) error {
 		go waitOne(ctx, tsk, errorCh)
 	}
 
+	runningTasks := tasksCount
+	var errList []error
 	for {
 		select {
 		case err := <-errorCh:
-			return err
+			runningTasks--
+			if err != nil {
+				// return immediately after receive first error if FailFast is set.
+				if options.FailFast {
+					return err
+				}
+				errList = append(errList, err)
+			} else {
+				return nil
+			}
 		case <-ctx.Done():
 			return fmt.Errorf("WaitAny %w", ctx.Err())
 		}
+
+		// are we finished yet?
+		if runningTasks == 0 {
+			break
+		}
 	}
+
+	// we have at least 1 error when FailFast is not set, return first one.
+	// caller can get error for individual task by using Wait(),
+	// it would return immediately after this WaitAny()
+	if len(errList) > 0 {
+		return errList[0]
+	}
+
+	// no error at all.
+	return nil
 }
--- wait_any_test.go
+++ wait_any_test.go
@@ -18,7 +18,7 @@ func TestWaitAny(t *testing.T) {
 	result := "something"
 	completedTsk := asynctask.NewCompletedTask(&result)
 
-	err := asynctask.WaitAny(ctx, countingTsk3, completedTsk)
+	err := asynctask.WaitAny(ctx, nil, countingTsk3, completedTsk)
 	elapsed := time.Since(start)
 	assert.NoError(t, err)
 	// should finish after right away
@@ -28,7 +28,7 @@ func TestWaitAny(t *testing.T) {
 	countingTsk2 := asynctask.Start(ctx, getCountingTask(10, "countingPer20ms", 20*time.Millisecond))
 	countingTsk3 = asynctask.Start(ctx, getCountingTask(10, "countingPer2ms", 2*time.Millisecond))
 	start = time.Now()
-	err = asynctask.WaitAny(ctx, countingTsk1, countingTsk2, countingTsk3)
+	err = asynctask.WaitAny(ctx, &asynctask.WaitAnyOptions{FailFast: true}, countingTsk1, countingTsk2, countingTsk3)
 	elapsed = time.Since(start)
 	assert.NoError(t, err)
 	cancelTaskExecution()
@@ -45,7 +45,49 @@ func TestWaitAnyErrorCase(t *testing.T) {
 	errorTsk := asynctask.Start(ctx, getErrorTask("expected error", 10*time.Millisecond))
 	result := "something"
 	completedTsk := asynctask.NewCompletedTask(&result)
-	err := asynctask.WaitAny(ctx, errorTsk, completedTsk)
+	err := asynctask.WaitAny(ctx, nil, errorTsk, completedTsk)
+	assert.NoError(t, err)
+	elapsed := time.Since(start)
+	// should finish after right away
+	assert.True(t, elapsed < 20*time.Millisecond, fmt.Sprintf("actually elapsed: %v", elapsed))
+	completedTskState := completedTsk.State()
+	assert.Equal(t, asynctask.StateCompleted, completedTskState, "completed task should finished")
+
+	countingTsk := asynctask.Start(ctx, getCountingTask(10, "countingPer40ms", 40*time.Millisecond))
+	errorTsk = asynctask.Start(ctx, getErrorTask("expected error", 10*time.Millisecond))
+	panicTsk := asynctask.Start(ctx, getPanicTask(20*time.Millisecond))
+	err = asynctask.WaitAny(ctx, nil, countingTsk, errorTsk, panicTsk)
+	// there is a succeed task
+	assert.NoError(t, err)
+	elapsed = time.Since(start)
+
+	countingTskState := countingTsk.State()
+	panicTskState := panicTsk.State()
+	errTskState := errorTsk.State()
+	cancelTaskExecution() // all assertion variable captured, cancel counting task
+
+	// should only finish after longest task.
+	assert.True(t, elapsed >= 40*10*time.Millisecond, fmt.Sprintf("actually elapsed: %v", elapsed))
+
+	assert.Equal(t, asynctask.StateCompleted, countingTskState, "countingTask should NOT finished")
+	assert.Equal(t, asynctask.StateFailed, errTskState, "error task should failed")
+	assert.Equal(t, asynctask.StateFailed, panicTskState, "panic task should Not failed")
+
+	// counting task do testing.Logf in another go routine
+	// while testing.Logf would cause DataRace error when test is already finished: https://github.com/golang/go/issues/40343
+	// wait minor time for the go routine to finish.
+	time.Sleep(1 * time.Millisecond)
+}
+
+func TestWaitAnyErrorWithFailFastCase(t *testing.T) {
+	t.Parallel()
+	ctx, cancelTaskExecution := newTestContextWithTimeout(t, 3*time.Second)
+
+	start := time.Now()
+	errorTsk := asynctask.Start(ctx, getErrorTask("expected error", 10*time.Millisecond))
+	result := "something"
+	completedTsk := asynctask.NewCompletedTask(&result)
+	err := asynctask.WaitAny(ctx, &asynctask.WaitAnyOptions{FailFast: true}, errorTsk, completedTsk)
 	assert.NoError(t, err)
 	elapsed := time.Since(start)
 	// should finish after right away
@@ -54,7 +96,7 @@ func TestWaitAnyErrorCase(t *testing.T) {
 	countingTsk := asynctask.Start(ctx, getCountingTask(10, "countingPer40ms", 40*time.Millisecond))
 	errorTsk = asynctask.Start(ctx, getErrorTask("expected error", 10*time.Millisecond))
 	panicTsk := asynctask.Start(ctx, getPanicTask(20*time.Millisecond))
-	err = asynctask.WaitAny(ctx, countingTsk, errorTsk, panicTsk)
+	err = asynctask.WaitAny(ctx, &asynctask.WaitAnyOptions{FailFast: true}, countingTsk, errorTsk, panicTsk)
 	assert.Error(t, err)
 	completedTskState := completedTsk.State()
 	assert.Equal(t, asynctask.StateCompleted, completedTskState, "completed task should finished")
